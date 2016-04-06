569fcb4fddb95ea3a4aae2d2d90d58b3754fd647
etharion <warlordcp@gmail.com>
2016-04-06 09:40:47 +0200

    data + exceptions

---
+++ src/Persistence/DataAccess.java
@@ -0,0 +1,15 @@
+package Persistence;
+
+import java.sql.Connection;
+
+import exceptions.PersistenceCommitFailureException;
+
+public interface DataAccess {
+
+	public Connection getConnection();
+	public void commit() throws PersistenceCommitFailureException;
+	public void rollback();
+	public void close();	
+	public void setAutoCommit(boolean bool);
+	
+}
